dcc7c9c20c1645647ab48fad60c0621b05b2c7f9
Dan Conrad <dan.m.conrad@gmail.com>
2017-10-14 12:45:26 -0500

    Completes the final site copy. Replaces the favicon

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -11,21 +11,15 @@ const Body = (props) => (
       <h2>Who Am I?</h2>
 
       <p>
-        THIS WHOLE SECTION NEEDS TO BE MORE WARM. I am a lifelong technology
-        enthusiast, software fanatic, and overall computer dork.
+        I'm a lifelong technology enthusiast, software fanatic, and overall computer dork.
       </p>
 
       <p>
-        Strong technical leader with a focus on lean and agile approaches to
-        solving today's product challenges in web development. Innate ability
-        to quickly identify underlying business requirements and build
-        efficient, scalable, and secure web architectures.
+        <strong>Professionally</strong>, I'm a full stack engineer and strong technical leader with a focus on lean and agile approaches to solving today's product challenges. I have a wide range of experience, and an innate ability to quickly identify underlying business requirements and build efficient, scalable, and secure web architectures. I have an extremely vigilant eye for design implementation and adherence to web best-practices, accompanied by an ability to construct standards to be used by other developers.
       </p>
 
       <p>
-        Extremely vigilant eye for design implementation and adherence to web
-        best-practices, accompanied by ability to construct standards to be
-        used by other developers.
+        <strong>Personally</strong>, I'm a tinkerer, foodie, and volunteer. I spend most of my free time split between woodworking and building furniture, hacking together robots and random electronics, making cheese and experimenting with new cooking techniques, and teaching kids how to code.
       </p>
     </section>
 
@@ -37,26 +31,18 @@ const Body = (props) => (
           <Icon type="medium" className={styles.Icon} />
         </div>
         <div>
-          <a href="#THELINK" className={styles.workItemTitle}>
-            Using a PS4 Controller to Move a Robot
+          <a href="https://goo.gl/VEs2mW" className={styles.workItemTitle}>
+            DIY Minimalist Weather Display (ReactJS + RaspberryPi)
           </a>
-          <p>I created a python package that greatly simplifies interacting with
-            a PS4 controller in any project. This is part of an ongoing series
-            to teach a robot how to self navigate.</p>
-        </div>
-      </div>
-
-      <div className={styles.workItem}>
-        <div className={styles.workItemIconContainer}>
-          <Icon type="medium" className={styles.Icon} />
-        </div>
-        <div>
-          <a href="#THELINK" className={styles.workItemTitle}>
-            DIY Minimalist Weather Display (built in ReactJS and RaspberryPi)
-          </a>
-          <p>I created a python package that greatly simplifies interacting with
-            a PS4 controller in any project. This is part of an ongoing series
-            to teach a robot how to self navigate.</p>
+          <p>
+            Weather monitors are all over the place. They're one of those
+            little projects that countless people post on Reddit. Some have
+            integrations with Google Calendars, Twitter accounts, and a ton of
+            other neat features. But there's always one major gripe I have
+            with them: they're way too busy, and impossible to read from a
+            few feat away. I wanted one that I could see and understand from my
+            bed…
+          </p>
         </div>
       </div>
     </section>
