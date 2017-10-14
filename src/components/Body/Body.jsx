29803f391e717e842f8abf486f88494798a2416f
Dan Conrad <dan.m.conrad@gmail.com>
2017-10-14 15:58:00 -0500

    Fixes mobile link padding. Correct grammar

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -32,11 +32,11 @@ class Body extends Component {
           </p>
 
           <p>
-            <strong>Professionally</strong>, I'm a full stack engineer and strong technical leader with a focus on lean and agile approaches to solving today's product challenges. I have a wide range of experience, and an innate ability to quickly identify underlying business requirements and build efficient, scalable, and secure web architectures. I have an extremely vigilant eye for design implementation and adherence to web best-practices, accompanied by a drive to construct team coding standards.
+            <strong>Professionally</strong>, I'm a full stack engineer and strong technical leader with a focus on lean and agile approaches to solving today's product challenges. I have a wide range of experience and an innate ability to quickly identify underlying business requirements to build efficient, scalable, and secure web architectures. I have an extremely vigilant eye for design implementation and adherence to web best-practices, accompanied by a drive to construct team coding standards.
           </p>
 
           <p>
-            <strong>Personally</strong>, I'm a tinkerer, foodie, and volunteer. I spend most of my free time split between woodworking and building furniture, hacking together robots and random electronics, making cheese and experimenting with new cooking techniques, and teaching kids how to code.
+            <strong>Personally</strong>, I'm a tinkerer, foodie, and volunteer. I spend most of my free time woodworking and building furniture, hacking together robots and random electronics, making cheese and experimenting with new cooking techniques, and teaching kids how to code.
           </p>
         </section>
 
